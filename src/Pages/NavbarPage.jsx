8d2c19cc947887e618dae7aec2623e4baae36ab4
ViNu-23 <vijayvinu46@gmail.com>
2024-08-30 21:06:49 +0530

    bell icon

--- src/Pages/NavbarPage.jsx
+++ src/Pages/NavbarPage.jsx
@@ -110,16 +110,34 @@ export default function NavbarPage() {
           </li>
         )}
         {user ? (
-          <Link
-            to="/profile"
-            className="text-white font-semibold h-7 w-8 rounded-full cursor-pointer "
-          >
-            <img
-              src={user.avatar}
-              alt="img"
-              className="object-contain rounded-full"
-            />
-          </Link>
+          <>
+            <div className="relative cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="text-sky-400 text-sm font-bold absolute -top-2 -right-1">0</span>
+            </div>
+
+            <Link
+              to="/profile"
+              className="text-white font-semibold h-7 w-8 rounded-full cursor-pointer flex items-center "
+            >
+              <img
+                src={user.avatar}
+                alt="img"
+                className="object-contain rounded-full"
+              />
+            </Link>
+          </>
         ) : (
           <li>
             <Link
@@ -137,7 +155,7 @@ export default function NavbarPage() {
             <div className="flex justify-center mt-3">
               <li className="w-96 flex items-center relative">
                 <input
-                placeholder="Search..."
+                  placeholder="Search..."
                   type="text"
                   className=" bg-slate-800 rounded-full px-5 py-2 w-full text-white"
                 />
